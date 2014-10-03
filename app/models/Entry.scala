b91635e4f3ec090296f71c613656e424fbf2f3ac
Artur Gajewski <artur.gajewski@fraktio.fi>
2014-10-03 21:38:32 +0100

    Refactored indentation

--- app/models/Entry.scala
+++ app/models/Entry.scala
@@ -10,46 +10,46 @@ case class Entry(id: Long, author: String, message: String, shortUrl: String)
 
 object Entry {
 
-    val entry = {
-        get[Long]("id") ~
-        get[String]("author") ~
-        get[String]("message") ~
-        get[String]("shortUrl") map {
-            case id~author~message~shortUrl => Entry(id, author, message, shortUrl)
-        }
+  val entry = {
+    get[Long]("id") ~
+    get[String]("author") ~
+    get[String]("message") ~
+    get[String]("shortUrl") map {
+        case id~author~message~shortUrl => Entry(id, author, message, shortUrl)
+    }
+  }
+
+  def all(): List[Entry] = DB.withConnection { implicit c =>
+    SQL("select * from entry").as(entry *)
+  }
+
+  def findByShortUrl(shortUrl: String) = DB.withConnection { implicit c =>
+    SQL("select * from entry where shortUrl = {shortUrl}").on(
+      'shortUrl -> shortUrl
+    ).as(entry *)
+  }
+
+  def create(entry: Entry) : String = {
+
+    val shortUrl = Random.alphanumeric.take(7).mkString
+
+    DB.withConnection { implicit c =>
+      SQL("insert into entry (author, message, shortUrl) values ({author}, {message}, {shortUrl})").on(
+        'author -> entry.author,
+        'message -> entry.message,
+        'shortUrl -> shortUrl
+      ).executeUpdate()
     }
 
-    def all(): List[Entry] = DB.withConnection { implicit c =>
-        SQL("select * from entry").as(entry *)
+    return shortUrl;
+  }
+
+  def delete(shortUrl: String) {
+    DB.withConnection { implicit c =>
+      SQL("delete from entry where shortUrl = {shortUrl}").on(
+        'shortUrl -> shortUrl
+      ).executeUpdate()
     }
-
-    def findByShortUrl(shortUrl: String) = DB.withConnection { implicit c =>
-        SQL("select * from entry where shortUrl = {shortUrl}").on(
-            'shortUrl -> shortUrl
-        ).as(entry *)
-    }
-
-    def create(entry: Entry) : String = {
-
-        val shortUrl = Random.alphanumeric.take(7).mkString
-
-        DB.withConnection { implicit c =>
-            SQL("insert into entry (author, message, shortUrl) values ({author}, {message}, {shortUrl})").on(
-                'author -> entry.author,
-                'message -> entry.message,
-                'shortUrl -> shortUrl
-            ).executeUpdate()
-        }
-
-        return shortUrl;
-    }
-
-    def delete(shortUrl: String) {
-        DB.withConnection { implicit c =>
-            SQL("delete from entry where shortUrl = {shortUrl}").on(
-                'shortUrl -> shortUrl
-            ).executeUpdate()
-        }
-    }
+  }
 
 }
