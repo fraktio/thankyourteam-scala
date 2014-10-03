b47ea2ab9109093fb16a3309aa1ce07c054cd31b
Artur Gajewski <artur.gajewski@fraktio.fi>
2014-10-03 10:19:05 +0100

    Initial commit

---
+++ app/controllers/Application.scala
@@ -0,0 +1,44 @@
+package controllers
+
+import play.api._
+import play.api.mvc._
+import play.api.data._
+import play.api.data.Forms._
+
+import models.Entry
+
+object Application extends Controller {
+
+    val entryForm = Form(
+        mapping(
+            "id" -> ignored(23L),
+            "author" -> text,
+            "message" -> text,
+            "shortUrl" -> ignored("")
+        )(Entry.apply)(Entry.unapply)
+    )
+
+    def entries = Action {
+        Ok(views.html.index(Entry.all(), entryForm))
+    }
+
+    def newEntry = Action { implicit request =>
+        entryForm.bindFromRequest.fold(
+            errors => BadRequest(views.html.index(Entry.all(), errors)),
+            entry => {
+                val identifier = Entry.create(entry)
+                Ok(views.html.view(Entry.findByShortUrl(identifier)))
+            }
+        )
+    }
+
+    def deleteEntry(shortUrl: String) = Action { implicit request =>
+        Entry.delete(shortUrl)
+        Redirect(routes.Application.entries)
+    }
+
+    def viewEntry(shortUrl: String) = Action { implicit request =>
+        Ok(views.html.view(Entry.findByShortUrl(shortUrl)))
+    }
+
+}
---
+++ app/models/Entry.scala
@@ -0,0 +1,57 @@
+package models
+
+import anorm._
+import anorm.SqlParser._
+import play.api.db._
+import play.api.Play.current
+import scala.util.Random
+
+case class Entry(id: Long, author: String, message: String, shortUrl: String)
+
+object Entry {
+
+    val entry = {
+        get[Long]("id") ~
+        get[String]("author") ~
+        get[String]("message") ~
+        get[String]("shortUrl") map {
+            case id~author~message~shortUrl => Entry(id, author, message, shortUrl)
+        }
+    }
+
+    def all(): List[Entry] = DB.withConnection { implicit c =>
+        SQL("select * from entry").as(entry *)
+    }
+
+    def findByShortUrl(shortUrl: String) = DB.withConnection { implicit c =>
+        SQL("select * from entry where shortUrl = {shortUrl}").on(
+            'shortUrl -> shortUrl
+        ).as(entry *)
+    }
+
+    def create(entry: Entry) : String = {
+
+        val shortUrl = Random.alphanumeric.take(7).mkString
+
+        System.out.println(shortUrl);
+
+        DB.withConnection { implicit c =>
+            SQL("insert into entry (author, message, shortUrl, created) values ({author}, {message}, {shortUrl})").on(
+                'author -> entry.author,
+                'message -> entry.message,
+                'shortUrl -> shortUrl
+            ).executeUpdate()
+        }
+
+        return shortUrl;
+    }
+
+    def delete(shortUrl: String) {
+        DB.withConnection { implicit c =>
+            SQL("delete from entry where shortUrl = {shortUrl}").on(
+                'shortUrl -> shortUrl
+            ).executeUpdate()
+        }
+    }
+
+}
