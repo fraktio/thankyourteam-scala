c2abff9ed0614821fe8afdd660b62adb48b121e7
Artur Gajewski <artur.gajewski@fraktio.fi>
2015-01-19 10:37:43 +0200

    Added stop and start scripts

---
+++ start.sh
@@ -0,0 +1,1 @@
+nohup ./thankyourteam-1.0-SNAPSHOT/bin/thankyourteam -DapplyEvolutions.default=true -Dhttp.port=9091 &
---
+++ stop.sh
@@ -0,0 +1,1 @@
+test -f thankyourteam-1.0-SNAPSHOT/RUNNING_PID && kill `cat thankyourteam-1.0-SNAPSHOT/RUNNING_PID` && sleep 5 && rm thankyourteam-1.0-SNAPSHOT/RUNNING_PID;
